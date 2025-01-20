8b0fb40a843c3e4c592ce13b5f8c6dc147750054
Martin <128410413+grapkos@users.noreply.github.com>
2025-01-20 19:37:23 +0100

    Add files via upload

---
+++ client/misc.lua
@@ -0,0 +1,7 @@
+RegisterCommand('clear', function(source, args)
+    TriggerEvent('chat:clear')
+end, false)
+
+RegisterCommand('c', function(source, args)
+    TriggerEvent('chat:clear')
+end, false)
