6fb8050ebc8ffd063438a68d7c52a0f53c9482c2
Martin <128410413+grapkos@users.noreply.github.com>
2025-01-21 16:17:15 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,5 @@
 SetConvarServerInfo('tags', 'GRAPCORE')
 
 if ( GetCurrentResourceName() ~= "grapcore" ) then
-  print( "^1ERROR: ^5Make sure resource name is set to ^4grapcore^5. ^0\n\n" )
+  print( "^1ERROR: ^5Make sure the resource name is set to ^4grapcore^5. ^0\n\n" )
 end
