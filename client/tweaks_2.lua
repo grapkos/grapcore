ee85105393ba7193ebc02da838b8ab81ef5e8e03
grapkos <128410413+grapkos@users.noreply.github.com>
2025-01-26 18:03:09 +0100

    Fixed Ragdoll Not Working

--- client/tweaks_2.lua
+++ client/tweaks_2.lua
@@ -162,7 +162,7 @@ end
 
 
 if Config.EnableRagdoll then
-    RegisterKeyMapping("ragdoll", "Toggle Ragdoll", "keyboard", 'K')
+    RegisterKeyMapping("ragdoll", "Toggle Ragdoll", "keyboard", 'U')
 
     local ragdoll = false
     function setRagdoll(flag)
