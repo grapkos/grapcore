7eb890aa6dcd17f64be5f7231a69c4bc3ecd9d1c
grapkos <128410413+grapkos@users.noreply.github.com>
2025-01-26 18:02:27 +0100

    Fixed Hide Hud Reticle for Marskman Rifle MK2

--- client/tweaks_3.lua
+++ client/tweaks_3.lua
@@ -191,6 +191,8 @@ if Config.HideWeaponReticle then
                 isSniper = true
             elseif currentWeaponHash == GetHashKey('WEAPON_HEAVYSNIPER_MK2') then
                 isSniper = true
+            elseif currentWeaponHash == GetHashKey('WEAPON_MARKSMANRIFLE_MK2') then
+                isSniper = true
             else
                 isSniper = false
             end
