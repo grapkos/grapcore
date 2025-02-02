67f57037da1a104508ccdea3a73f2218bf975552
grapkos <128410413+grapkos@users.noreply.github.com>
2025-02-02 14:06:42 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author 'grapkos' 
 description 'Miscellaneous, enhancing game experience'
-version '2.0.0 <2025>'
+version '2.0.1'
 
 data_file 'VEHICLE_METADATA_FILE' 'handling.meta'
 
